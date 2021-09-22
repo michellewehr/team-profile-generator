98fc531d9ae4ac543e3fcb2c5c84e64f3351f905
Michelle Wehr <michelle_wehr@icloud.com>
2021-09-22 10:15:33 -0400

    added class constructor for employee and extended it to manager

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,10 @@
 const Employee = require('../lib/Employee.js');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Dave');
+    const employee = new Employee('Dave', 5, 'email@email', 43);
 
     expect(employee.name).toBe('Dave');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.office).toEqual(expect.any(Number));
 })

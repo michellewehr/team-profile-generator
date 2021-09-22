73ad9cf4275f9041a698ac67f8ed2ed4556b15ab
Michelle Wehr <michelle_wehr@icloud.com>
2021-09-22 11:53:06 -0400

    made getName methods into inquire prompts

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,20 @@
+const { test, expect } = require('@jest/globals');
 const Employee = require('../lib/Employee.js');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Dave', 5, 'email@email', 43);
+    const employee = new Employee('Dave', 5, 'email@email');
 
     expect(employee.name).toBe('Dave');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
-    expect(employee.office).toEqual(expect.any(Number));
 })
+
+// test('gets employee name value', () => {
+//     const employee = new Employee('Dave');
+
+//     expect(employee.getName()).toBe('Dave');
+// })
+
+// test('gets employees id value', () => {
+//     const 
+// })
--- index.js
+++ index.js
@@ -1,50 +1,49 @@
 const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 
-function getManager() {
-    inquirer  
-        .prompt([
-            {
-            type: 'text',
-            name: 'name',
-            message: "What is your team manager's name?"
-            },
-            {
-            type: 'text',
-            name: 'id',
-            message: "What is your manager's employee ID?"
-            },
-            {
-            type: 'text',
-            name: 'email',
-            message: "What is your manager's email address?"
-            },
-            {
-            type: 'text',
-            name: 'office',
-            message: "What is your manager's office number?"
-            }
-        ])
-        .then(managerInfo => {
-            console.log(new Manager(managerInfo.name, managerInfo.id, managerInfo.email, managerInfo.office))
-        })
-}
+// function questions() {
+//     return inquirer  
+//         .prompt([
+//             {
+//             type: 'text',
+//             name: 'name',
+//             message: "What is your team members's name?"
+//             },
+//             {
+//             type: 'text',
+//             name: 'id',
+//             message: "What is your team member's employee ID?"
+//             },
+//             {
+//             type: 'text',
+//             name: 'email',
+//             message: "What is your team members's email address?"
+//             },
+//         ])
+//         .then(data => {
+//            return new Employee(data.name, data.id, data.email);
+//         })
+// }
 
-function confirmAdd(managerData) {
-    inquirer
-    .prompt({
-        type: 'confirm',
-        name: 'confirmAdd',
-        message: "Would you like to add another team member to your team's profile?"
-    })
-    .then(confirmData => {
-        if (confirmData.confirmAdd) {
-            console.log('need addMember function to run here');
-        } else {
-            return managerData;
-        }
-    })
-}
+// function confirmAdd(managerData) {
+//     inquirer
+//     .prompt({
+//         type: 'confirm',
+//         name: 'confirmAdd',
+//         message: "Would you like to add another team member to your team's profile?"
+//     })
+//     .then(confirmData => {
+//         if (confirmData.confirmAdd) {
+//             console.log('need addMember function to run here');
+//         } else {
+//             return managerData;
+//         }
+//     })
+// }
 
-getManager();
+let employee = new Employee;
+console.log(employee);
+
+
 

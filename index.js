4a192724857262ad3f8ffabb4f2407c62110d4a9
Michelle Wehr <michelle_wehr@icloud.com>
2021-09-22 12:14:44 -0400

    created a team array and was able to add new employee to team

--- index.js
+++ index.js
@@ -1,49 +1,56 @@
+const { template } = require('@babel/core');
 const inquirer = require('inquirer');
 const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 
-// function questions() {
-//     return inquirer  
-//         .prompt([
-//             {
-//             type: 'text',
-//             name: 'name',
-//             message: "What is your team members's name?"
-//             },
-//             {
-//             type: 'text',
-//             name: 'id',
-//             message: "What is your team member's employee ID?"
-//             },
-//             {
-//             type: 'text',
-//             name: 'email',
-//             message: "What is your team members's email address?"
-//             },
-//         ])
-//         .then(data => {
-//            return new Employee(data.name, data.id, data.email);
-//         })
-// }
+const team = [];
 
-// function confirmAdd(managerData) {
-//     inquirer
-//     .prompt({
-//         type: 'confirm',
-//         name: 'confirmAdd',
-//         message: "Would you like to add another team member to your team's profile?"
-//     })
-//     .then(confirmData => {
-//         if (confirmData.confirmAdd) {
-//             console.log('need addMember function to run here');
-//         } else {
-//             return managerData;
-//         }
-//     })
-// }
+//prompt questions about employee
+function questions() {
+    return inquirer  
+        .prompt([
+            {
+            type: 'text',
+            name: 'name',
+            message: "What is your team members's name?"
+            },
+            {
+            type: 'text',
+            name: 'id',
+            message: "What is your team member's employee ID?"
+            },
+            {
+            type: 'text',
+            name: 'email',
+            message: "What is your team members's email address?"
+            },
+        ])
+        .then(function(data) {
+           const employee = new Employee(data.name, data.id, data.email);
+           team.push(employee);
+           return team;
+        })
+        .then(function(team) {
+            console.log(team);
+        })
+}
 
-let employee = new Employee;
-console.log(employee);
+function confirmAdd(managerData) {
+    inquirer
+    .prompt({
+        type: 'confirm',
+        name: 'confirmAdd',
+        message: "Would you like to add another team member to your team's profile?"
+    })
+    .then(confirmData => {
+        if (confirmData.confirmAdd) {
+            console.log('need addMember function to run here');
+        } else {
+            return managerData;
+        }
+    })
+}
 
+questions();
 
 
